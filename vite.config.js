9a10b1b156831cce13b27bf60b1280ac4a550dd9
bastienlepreux <81980800+bastienlepreux@users.noreply.github.com>
2025-10-14 22:44:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: change base to '/<nom-du-repo>/' before pushing to GitHub Pages project site
 export default defineConfig({
   plugins: [react()],
-  base: '/akw/', // ex: '/a-kaz-aw/' ; si ton dépôt = username.github.io, remplace par '/'
+  base: '/akw/',   // ← important pour GitHub Pages
 })
